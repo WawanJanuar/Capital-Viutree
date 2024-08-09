9cb1f38f1d25811087a4f80bcb4f83c12700b215
WawanJanuar <wawanjanuar547@gmail.com>
2024-08-10 00:16:03 +0700

    perbaikan bug dan penambahan animasi kursor

--- js/kursor.js
+++ js/kursor.js
@@ -1,1 +1,11 @@
-//penghapusan animasi kursor
+document.addEventListener('click', function(e) {
+    const effect = document.createElement('div');
+    effect.className = 'click-effect';
+    effect.style.left = `${e.pageX - 7.5}px`;
+    effect.style.top = `${e.pageY - 7.5}px`;
+    document.body.appendChild(effect);
+
+    setTimeout(() => {
+        effect.remove();
+    }, 300);  // Durasi animasi harus sesuai dengan @keyframes expand
+});
